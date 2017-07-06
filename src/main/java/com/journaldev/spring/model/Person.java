33d6304c1da78959ff2eb6b0353028d924bf3d75
CesarLopez <cesar.1608@outlook.com>
2017-07-06 01:17:35 -0500

    puse mi comentario =D

--- src/main/java/com/journaldev/spring/model/Person.java
+++ src/main/java/com/journaldev/spring/model/Person.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+//CESAR LOPEZ
+
 /**
  * Entity bean with JPA annotations
  * Hibernate provides JPA implementation
